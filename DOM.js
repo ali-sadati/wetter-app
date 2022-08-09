cd968d38640d0578419caf719f221d30bd39d678
Ali Sadati <ali.sadati.sm@gmail.com>
2022-08-09 12:25:18 +0200

    validation

--- DOM.js
+++ DOM.js
@@ -1,13 +1,22 @@
 let accardion = document.querySelector(".accardion")
-
 Array.from(accardion.children).forEach(wrapper => {
-    wrapper.querySelector("span").addEventListener("click", e =>{
+  if (wrapper.classList.contains("show")) {
+      wrapper.querySelector("div").style.maxHeight = wrapper.querySelector("div").scrollHeight + 30 + 'px'
+   }
+    wrapper.querySelector("span").addEventListener("click", e => {
         let span = e.target
         let wrapper = span.parentElement
+        let div = span.nextElementSibling
         wrapper.classList.toggle("show")
+        if (wrapper.classList.contains("show")) {
+            div.style.maxHeight = div.scrollHeight + 30 + 'px'
+        } else {
+            div.style.maxHeight = null
+        }
         Array.from(accardion.children).forEach(w => {
             if (w != wrapper) {
                 w.classList.remove("show")
+                w.querySelector('div').style.maxHeight = null
             }
         })
     })
